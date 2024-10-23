41419f0c9f4f1c2f58a2f878582021cce47805d8
mubarak.muhammed <mubarak.muhammed@wemabank.com>
2024-10-23 17:46:30 +0100

    footer component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import React from "react";
-import Navigation from "./components/navigation/Navigation";
+import Navigation from "components/navigation/Navigation";
 import Header from "components/header/Header";
+import Footer from "components/footer/Footer";
 
 function App() {
   return (
     <div>
       <Navigation />
       <Header />
+      <div className="mt-10"></div>
+      <Footer />
     </div>
   );
 }
